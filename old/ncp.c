f185358f48f8c1472eed6d8410153b34135a167f
John B An <jan10@ugrad1.cs.jhu.edu>
2014-09-16 03:20:10 -0400

    copied over ucast, both can send and receive, can send packets

--- old/ncp.c
+++ old/ncp.c
@@ -6,28 +6,28 @@ int gethostname(char*,size_t);
 
 void PromptForHostName( char *my_name, char *host_name, size_t max_len ); 
 
+/**added**/
+void split_string(char *destination, char *dest_file_name, char *dest_comp_name);
+
 int main(int argc, char **argv)
 {
-    struct sockaddr_in    name;
     struct sockaddr_in    send_addr;
-    struct sockaddr_in    from_addr;
-    socklen_t             from_len;
     struct hostent        h_ent;
     struct hostent        *p_h_ent;
     char                  host_name[NAME_LENGTH] = {'\0'};
     char                  my_name[NAME_LENGTH] = {'\0'};
     int                   host_num;
-    int                   from_ip;
-    int                   ss,sr;
+    int                   ss;
     fd_set                mask;
     fd_set                dummy_mask,temp_mask;
     int                   bytes;
     int                   num;
-    struct packet         *send_buf;
-    char                  ack[WINDOW_SIZE];
     struct timeval        timeout;
 
-    /**added**/
+    /*** NON COPIED CODE ***/
+    struct packet *send_buffer = malloc(WINDOW_SIZE * sizeof(struct packet));
+    /*char *acks = malloc(WINDOW_SIZE * sizeof(char));*/
+    
     int loss_rate = atoi(argv[1]);
     char *filename = argv[2];
     char *destination = argv[3];
@@ -35,31 +35,16 @@ int main(int argc, char **argv)
     char *dest_comp_name;
 
     int i;
-    int nread;
+    /*int nread;*/
     int ret;
 
     FILE *fr;
 
     sendto_dbg_init(loss_rate);
+    /********************/
 
-    /****************************
-     * **************
-     ****************************/
-    sr = socket(AF_INET, SOCK_DGRAM, 0);  /* socket for receiving (udp) */
-    if (sr<0) {
-        perror("Ucast: socket");
-        exit(1);
-    }
+/*** Socket Initialization (I think) ***/
 
-    name.sin_family = AF_INET; 
-    name.sin_addr.s_addr = INADDR_ANY; 
-    name.sin_port = htons(PORT);
-
-    if ( bind( sr, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
-        perror("Ucast: bind");
-        exit(1);
-    }
- 
     ss = socket(AF_INET, SOCK_DGRAM, 0); /* socket for sending (udp) */
     if (ss<0) {
         perror("Ucast: socket");
@@ -83,18 +68,17 @@ int main(int argc, char **argv)
 
     FD_ZERO( &mask );
     FD_ZERO( &dummy_mask );
-    FD_SET( sr, &mask );
+    /*FD_SET( sr, &mask );*/
     FD_SET( (long)0, &mask ); /* stdin */
-    /****************************
-     * **************
-     ****************************/
 
-    send_buf = malloc(WINDOW_SIZE * sizeof(struct packet));
+/**************/
+
+    /**ADDED CODE**********/
+
     /*split_string(destination, &dest_file_name, &dest_comp_name);*/
 
 /*    printf("-------------------\nDestination:%s\nDest-File-Name:%s\nDest-Comp-Name:%s\n", destination, &dest_file_name, &dest_comp_name);
     printf("-------------------\nDest-File-Name:%s\n", &dest_file_name);*/
-
     if ((fr = fopen(filename, "r")) == NULL) {
         perror("fopen");
         exit(0);
@@ -105,52 +89,34 @@ int main(int argc, char **argv)
 
     /** our code **/
 
+    int nread;
     int z;
-
     for (z = 0; z < 16; z++) {
-        printf("HI %d\n", z);
-        nread = fread(send_buf[z].payload, 1, PAYLOAD_SIZE, fr);
-        sendto_dbg(ss, send_buf[z].payload, PAYLOAD_SIZE, 0,
+        nread = fread(send_buffer[z].payload, 1, PAYLOAD_SIZE, fr);
+        sendto_dbg(ss, send_buffer[z].payload, PAYLOAD_SIZE, 0,
             (struct sockaddr *)&send_addr, sizeof(send_addr));
-
     }
 
-   /* 
-    for(;;)
+/*    for(;;)
     {
         temp_mask = mask;
         timeout.tv_sec = 10;
-	timeout.tv_usec = 0;
+	    timeout.tv_usec = 0;
         num = select( FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, &timeout);
         if (num > 0) {
-            if ( FD_ISSET( sr, &temp_mask) ) {
-                from_len = sizeof(from_addr);
-                bytes = recvfrom( sr, mess_buf, sizeof(mess_buf), 0,  
-                          (struct sockaddr *)&from_addr, 
-                          &from_len );
-                mess_buf[bytes] = 0;
-                from_ip = from_addr.sin_addr.s_addr;
 
-                printf( "Received from (%d.%d.%d.%d): %s\n", 
-								(htonl(from_ip) & 0xff000000)>>24,
-								(htonl(from_ip) & 0x00ff0000)>>16,
-								(htonl(from_ip) & 0x0000ff00)>>8,
-								(htonl(from_ip) & 0x000000ff),
-								mess_buf );
-
-            }else if( FD_ISSET(0, &temp_mask) ) {
-                bytes = read( 0, input_buf, sizeof(input_buf) );
-                input_buf[bytes] = 0;
-                printf( "There is an input: %s\n", input_buf );
-                sendto( ss, input_buf, strlen(input_buf), 0, 
+                printf("HI");
+            if (FD_ISSET(0, &temp_mask) ) {
+                printf("HI2");
+                sendto_dbg( ss, send_buffer[0].payload, strlen(send_buffer[0].payload), 0, 
                     (struct sockaddr *)&send_addr, sizeof(send_addr) );
             }
 	} else {
 		printf(".");
 		fflush(0);
         }
-    }
-*/
+    }*/
+
     fclose(fr);
     return 0;
 
@@ -186,7 +152,6 @@ void split_string(char *destination, char *dest_file_name, char *dest_comp_name)
     memcpy(dest_comp_name, &destination[at_index+1], strlen(destination)-at_index-1); 
     dest_comp_name[strlen(destination)-at_index-1] = '\0';
 }
-
 
 void PromptForHostName( char *my_name, char *host_name, size_t max_len ) {
 
---
+++ old/rcv.c
@@ -0,0 +1,82 @@
+#include "net_include.h"
+
+#define NAME_LENGTH 80
+
+int main()
+{
+    struct sockaddr_in    name;
+    struct sockaddr_in    from_addr;
+    socklen_t             from_len;
+    int                   from_ip;
+    int                   sr;
+    fd_set                mask;
+    fd_set                dummy_mask,temp_mask;
+    int                   bytes;
+    int                   num;
+    struct timeval        timeout;
+
+    /**added**/
+    int i = 0;
+    struct packet *rcv_buffer = malloc(WINDOW_SIZE * sizeof(struct packet));
+    
+    /*********/
+    sr = socket(AF_INET, SOCK_DGRAM, 0);  /* socket for receiving (udp) */
+    if (sr<0) {
+        perror("Ucast: socket");
+        exit(1);
+    }
+
+    name.sin_family = AF_INET; 
+    name.sin_addr.s_addr = INADDR_ANY; 
+    name.sin_port = htons(PORT);
+
+    if ( bind( sr, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
+        perror("Ucast: bind");
+        exit(1);
+    }
+
+    FD_ZERO( &mask );
+    FD_ZERO( &dummy_mask );
+    FD_SET( sr, &mask );
+    FD_SET( (long)0, &mask ); /* stdin */
+    for(;;)
+    {
+        temp_mask = mask;
+        timeout.tv_sec = 10;
+	timeout.tv_usec = 0;
+        num = select( FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, &timeout);
+        if (num > 0) {
+            if ( FD_ISSET( sr, &temp_mask) ) {
+                from_len = sizeof(from_addr);
+                
+                /*bytes = recvfrom( sr, mess_buf, sizeof(mess_buf), 0,  
+                          (struct sockaddr *)&from_addr, 
+                          &from_len );
+                mess_buf[bytes] = 0;
+                */
+                
+                from_ip = from_addr.sin_addr.s_addr;
+
+
+                printf( "Received from (%d.%d.%d.%d): \n", 
+								(htonl(from_ip) & 0xff000000)>>24,
+								(htonl(from_ip) & 0x00ff0000)>>16,
+								(htonl(from_ip) & 0x0000ff00)>>8,
+								(htonl(from_ip) & 0x000000ff));
+                
+                printf("This is the index: %d\n", i);
+
+                recv( sr, rcv_buffer[i].payload, PAYLOAD_SIZE, 0 );
+
+                printf("%s\n", rcv_buffer[i].payload);
+                i++;
+            }
+	    } else {
+		    printf(".");
+		    fflush(0);
+        }
+    }
+
+    return 0;
+
+}
